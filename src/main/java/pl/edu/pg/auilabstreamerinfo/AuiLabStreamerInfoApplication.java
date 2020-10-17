9407f9d76246ac86bf87cc5f7d925ba601a691f9
Krzysztof Sajko <sajko.krzysztof@gmail.com>
2020-10-18 01:59:06 +0200

    Initial commit

---
+++ src/main/java/pl/edu/pg/auilabstreamerinfo/AuiLabStreamerInfoApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pg.auilabstreamerinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuiLabStreamerInfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuiLabStreamerInfoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/edu/pg/auilabstreamerinfo/AuiLabStreamerInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pg.auilabstreamerinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuiLabStreamerInfoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
