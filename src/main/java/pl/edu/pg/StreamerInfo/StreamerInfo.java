670c423b6097c1fe48da83540980e67494d55dec
Krzysztof Sajko <sajko.krzysztof@gmail.com>
2020-12-19 22:48:19 +0100

    Added CORS filter.

--- src/main/java/pl/edu/pg/StreamerInfo/StreamerInfo.java
+++ src/main/java/pl/edu/pg/StreamerInfo/StreamerInfo.java
@@ -7,6 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @SpringBootApplication
 public class StreamerInfo {
@@ -28,18 +34,33 @@ public class StreamerInfo {
                         .and()
                         .path("/api/genres", "/api/genres/{id}")
                         .uri(genresUrl))
-                .route("games", r -> r.host("localhost:8080")
+                .route("games", r -> r.host(hostUrl)
                         .and()
                         .path("/api/games", "/api/games/**",
                                 "/api/genres/{genreId}/games", "/api/genres/{genreId}/games/**",
                                 "/api/streamers/{streamerId}/games", "/api/streamers/{streamerId}/games/**")
                         .uri(gamesUrl))
-                .route("streamers", r -> r.host("localhost:8080")
+                .route("streamers", r -> r.host(hostUrl)
                         .and()
                         .path("/api/streamers", "/api/streamers/**",
                                 "/api/genres/{genreId}/streamers", "/api/genres/{genreId}/streamers/**",
                                 "/api/games/{gameId}/streamers", "/api/games/{gameId}/streamers/**")
                         .uri(streamersUrl))
                 .build();
+    }
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+
+        final CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Collections.singletonList("*"));
+        corsConfig.setMaxAge(3600L);
+        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "PUT"));
+        corsConfig.addAllowedHeader("*");
+
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
     }
 }
